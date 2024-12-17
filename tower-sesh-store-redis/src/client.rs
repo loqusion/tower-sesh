240d2cca0e4195db3bf7420bafa5a7fcd25f8122
loqusion <38332081+loqusion@users.noreply.github.com>
2024-12-17 00:52:46 -0600

    feat(store-redis): init

---
+++ tower-sesh-store-redis/src/client.rs
@@ -0,0 +1,64 @@
+use futures::FutureExt;
+use redis::{
+    aio::{ConnectionLike, ConnectionManager},
+    Client, Cmd, Pipeline, RedisFuture, RedisResult, Value,
+};
+
+/// A connection manager that immediately retries a request if it fails due
+/// to a dropped connection.
+///
+/// The default [`ConnectionManager`] behavior is to reconnect if a request
+/// fails due to a dropped connection, however that request's error is
+/// propagated to the caller instead of re-attempting the request.
+pub struct ConnectionManagerWithRetry(ConnectionManager);
+
+impl ConnectionManagerWithRetry {
+    pub(crate) async fn new(client: Client) -> RedisResult<Self> {
+        ConnectionManager::new(client).await.map(Self::from)
+    }
+}
+
+impl From<ConnectionManager> for ConnectionManagerWithRetry {
+    fn from(value: ConnectionManager) -> Self {
+        Self(value)
+    }
+}
+
+impl From<ConnectionManagerWithRetry> for ConnectionManager {
+    fn from(value: ConnectionManagerWithRetry) -> Self {
+        value.0
+    }
+}
+
+impl ConnectionLike for ConnectionManagerWithRetry {
+    fn req_packed_command<'a>(&'a mut self, cmd: &'a Cmd) -> RedisFuture<'a, Value> {
+        (async move {
+            match self.0.send_packed_command(cmd).await {
+                Err(err) if err.is_connection_dropped() => self.0.send_packed_command(cmd).await,
+                result @ (Err(_) | Ok(_)) => result,
+            }
+        })
+        .boxed()
+    }
+
+    fn req_packed_commands<'a>(
+        &'a mut self,
+        cmd: &'a Pipeline,
+        offset: usize,
+        count: usize,
+    ) -> RedisFuture<'a, Vec<Value>> {
+        (async move {
+            match self.0.send_packed_commands(cmd, offset, count).await {
+                Err(err) if err.is_connection_dropped() => {
+                    self.0.send_packed_commands(cmd, offset, count).await
+                }
+                result @ (Err(_) | Ok(_)) => result,
+            }
+        })
+        .boxed()
+    }
+
+    fn get_db(&self) -> i64 {
+        self.0.get_db()
+    }
+}
---
+++ tower-sesh-store-redis/src/lib.rs
@@ -0,0 +1,235 @@
+use std::borrow::Cow;
+
+use async_trait::async_trait;
+use client::ConnectionManagerWithRetry;
+use redis::{
+    aio::{ConnectionLike, ConnectionManager},
+    AsyncCommands, AsyncConnectionConfig, Client, ExistenceCheck, IntoConnectionInfo, RedisResult,
+    SetExpiry, SetOptions, ToRedisArgs,
+};
+use tower_sesh::{
+    session::{Record, SessionKey},
+    store::Error,
+    SessionStore,
+};
+
+// Required to use the redis asynchronous interface
+#[cfg(not(any(feature = "tokio-comp", feature = "async-std-comp")))]
+compile_error!("to use tower-sesh-store-redis, either the `tokio-comp` or `async-std-comp` feature must be enabled");
+
+pub mod client;
+
+const DEFAULT_KEY_PREFIX: &str = "session_";
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+pub struct RedisStore<C: ConnectionLike> {
+    connection: C,
+    config: RedisStoreConfig,
+}
+
+impl<C: ConnectionLike> std::fmt::Debug for RedisStore<C> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("hi")
+    }
+}
+
+struct RedisStoreConfig {
+    key_prefix: Cow<'static, str>,
+}
+
+impl Default for RedisStoreConfig {
+    fn default() -> Self {
+        Self {
+            key_prefix: Cow::Borrowed(DEFAULT_KEY_PREFIX),
+        }
+    }
+}
+
+impl RedisStore<ConnectionManagerWithRetry> {
+    /// Connect to a redis server and return a store.
+    ///
+    /// When opening a client a URL in the following format should be used:
+    ///
+    /// ```not_rust
+    /// redis://<host>:<port>/...
+    /// ```
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use tower_sesh_store_redis::RedisStore;
+    ///
+    /// # tokio_test::block_on(async {
+    /// let store = RedisStore::open("redis://127.0.0.1/").await?;
+    /// # Ok::<(), redis::RedisError>(())
+    /// # }).unwrap();
+    /// ```
+    pub async fn open<T: IntoConnectionInfo>(params: T) -> RedisResult<Self> {
+        let client = Client::open(params)?;
+        Self::with_client(client).await
+    }
+
+    /// Create a new redis store with the provided client.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use tower_sesh_store_redis::RedisStore;
+    ///
+    /// # tokio_test::block_on(async {
+    /// let client = redis::Client::open("redis://127.0.0.1/")?;
+    /// let store = RedisStore::with_client(client).await?;
+    /// # Ok::<(), redis::RedisError>(())
+    /// # }).unwrap();
+    /// ```
+    pub async fn with_client(client: Client) -> RedisResult<Self> {
+        let connection = ConnectionManagerWithRetry::new(client).await?;
+        Ok(Self {
+            connection,
+            config: RedisStoreConfig::default(),
+        })
+    }
+}
+
+impl<C: ConnectionLike> RedisStore<C> {
+    fn redis_key(&self, session_key: &SessionKey) -> String {
+        let mut redis_key =
+            String::with_capacity(self.config.key_prefix.len() + SessionKey::ENCODED_LEN);
+        redis_key.push_str(&self.config.key_prefix);
+        redis_key.push_str(&session_key.encode());
+        redis_key
+    }
+}
+
+impl<C: ConnectionLike> RedisStore<C>
+where
+    C: Clone,
+{
+    async fn connection(&self) -> Result<C> {
+        Ok(self.connection.clone())
+    }
+}
+
+#[async_trait]
+impl<C: ConnectionLike> SessionStore for RedisStore<C>
+where
+    C: Clone + Send + Sync + 'static,
+{
+    async fn create(&self, record: Record) -> Result<SessionKey> {
+        let mut conn = self.connection().await?;
+
+        let expiry = record.set_expiry();
+        let serialized = serialize(&record);
+
+        // Collision resolution
+        // (This is statistically improbable for a sufficiently large session key)
+        const MAX_RETRIES: usize = 4;
+        for _ in 0..MAX_RETRIES {
+            let session_key = SessionKey::generate();
+            let key = self.redis_key(&session_key);
+
+            let v: redis::Value = conn
+                .set_options(
+                    &key,
+                    &serialized,
+                    SetOptions::default()
+                        .conditional_set(ExistenceCheck::NX)
+                        .with_expiration(expiry),
+                )
+                .await
+                .map_err(|err| todo!())?;
+
+            match v {
+                redis::Value::Nil => {} // Conflict with NX: key exists
+                _ => return Ok(session_key),
+            }
+        }
+
+        Err(err_max_iterations_reached())
+    }
+
+    async fn load(&self, session_key: &SessionKey) -> Result<Option<Record>> {
+        let key = self.redis_key(session_key);
+        let mut conn = self.connection().await?;
+
+        const WEEK_IN_SECONDS: i64 = 60 * 60 * 24 * 7;
+        const DEFAULT_EXPIRY: i64 = 2 * WEEK_IN_SECONDS;
+
+        let (value, expire_time) = redis::pipe()
+            .atomic()
+            .expire(&key, DEFAULT_EXPIRY) // Ensure the key has a timeout if one isn't set
+            .arg("NX")
+            .ignore()
+            .get(&key)
+            .expire_time(&key)
+            .query_async::<(Option<String>, i64)>(&mut conn)
+            .await
+            .map_err(|err| todo!())?;
+
+        match value {
+            None => Ok(None),
+            Some(value) => Some(deserialize(&value, expire_time)).transpose(),
+        }
+    }
+
+    async fn update(&self, session_key: &SessionKey, record: Record) -> Result<()> {
+        let key = self.redis_key(session_key);
+        let mut conn = self.connection().await?;
+
+        let expiry = record.set_expiry();
+        let serialized = serialize(&record);
+
+        let _: () = conn
+            .set_options(
+                &key,
+                serialized,
+                SetOptions::default().with_expiration(expiry),
+            )
+            .await
+            .map_err(|err| todo!())?;
+
+        Ok(())
+    }
+
+    async fn delete(&self, session_key: &SessionKey) -> Result<()> {
+        let key = self.redis_key(session_key);
+        let mut conn = self.connection().await?;
+
+        let _: () = conn.del(&key).await.map_err(|err| todo!())?;
+
+        Ok(())
+    }
+}
+
+trait RecordExt {
+    fn set_expiry(&self) -> SetExpiry;
+}
+
+impl RecordExt for Record {
+    fn set_expiry(&self) -> SetExpiry {
+        SetExpiry::EXAT(todo!())
+    }
+}
+
+fn serialize(record: &Record) -> Vec<u8> {
+    todo!()
+}
+
+fn deserialize(s: &str, ttl: i64) -> Result<Record> {
+    debug_assert!(ttl >= 0, "ttl is negative. This is a bug.");
+    todo!()
+}
+
+struct RedisRecord {
+    data: (),
+}
+impl RedisRecord {
+    fn into_record(self, ttl: i64) -> Record {
+        todo!()
+    }
+}
+
+fn err_max_iterations_reached() -> Error {
+    todo!()
+}
