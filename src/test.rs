ecd0859745b094f4375094de362daded422b661b
loqusion <38332081+loqusion@users.noreply.github.com>
2024-12-19 17:38:52 -0600

    test: `test_suite` macro

--- src/test.rs
+++ src/test.rs
@@ -2,6 +2,8 @@ use std::sync::atomic::{self, AtomicU64};
 
 use crate::session::SessionKey;
 
+pub mod suite;
+
 /// A session key that is safe to use in tests without fear of collisions.
 ///
 /// Collisions can cause tests to be flaky, since two tests using the same
@@ -17,3 +19,35 @@ pub fn test_key() -> SessionKey {
     let v = unsafe { KEY_STATE.fetch_add(1, atomic::Ordering::SeqCst) as u128 };
     SessionKey::try_from(v).unwrap()
 }
+
+#[macro_export]
+macro_rules! test_suite {
+    (@suite $store_constr:expr, $cookie_content_security:expr) => {
+        #[::tokio::test]
+        async fn basic_workflow() {
+            $crate::test::suite::basic_workflow($store_constr, $cookie_content_security).await.unwrap();
+        }
+    };
+    ($store_constr:expr $(,)?) => {
+        /// Test suite using signed cookies.
+        mod signed_cookie {
+            use super::*;
+            $crate::test_suite!(
+                @suite
+                $store_constr,
+                $crate::config::CookieContentSecurity::Signed
+            );
+        }
+
+        /// Test suite using private cookies.
+        mod private_cookie {
+            use super::*;
+            $crate::test_suite!(
+                @suite
+                $store_constr,
+                $crate::config::CookieContentSecurity::Private
+            );
+        }
+    };
+}
+pub use test_suite;
---
+++ src/test/suite.rs
@@ -0,0 +1,18 @@
+use axum::{routing::method_routing, Router};
+use axum_test::TestServer;
+
+use crate::{config::CookieContentSecurity, SessionStore};
+
+pub async fn basic_workflow(
+    store: impl SessionStore + 'static,
+    policy: CookieContentSecurity,
+) -> anyhow::Result<()> {
+    let app = Router::new().route("/test", method_routing::get(|| async { "hi" }));
+    let server = TestServer::builder()
+        .http_transport()
+        .expect_success_by_default()
+        .save_cookies()
+        .build(app)?;
+
+    Ok(())
+}
