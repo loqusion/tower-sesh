40c517d078a01ea75569a38064812b4c9ad3677b
loqusion <loqusion@gmail.com>
2025-02-02 19:38:33 -0600

    docs(core): document crate

--- tower-sesh-core/src/lib.rs
+++ tower-sesh-core/src/lib.rs
@@ -1,3 +1,17 @@
+//! Core of `tower-sesh`.
+//!
+//! # Warning
+//!
+//! The API of this crate is not meant for general use and does _not_ follow
+//! Semantic Versioning. If you are building a custom session store, you should
+//! pin an exact version of `tower-sesh-core` to avoid breakages:
+//!
+//! ```not_rust
+//! tower-sesh-core = { version = "=X.Y.Z" }
+//! ```
+//!
+//! And then keep releases in sync with `tower-sesh-core`.
+
 #[doc(inline)]
 pub use crate::key::SessionKey;
 #[doc(inline)]
