2279222050f26a805722bc83a90ad8ff4418a7eb
loqusion <loqusion@gmail.com>
2025-02-13 19:48:38 -0600

    docs(store-redis): change reseed threshold in docs

--- tower-sesh-store-redis/src/lib.rs
+++ tower-sesh-store-redis/src/lib.rs
@@ -142,7 +142,7 @@ impl<T, C: GetConnection, R: TryCryptoRng> RedisStore<T, C, R> {
     /// # type SessionData = ();
     /// #
     /// # tokio_test::block_on(async {
-    /// const RESEED_THRESHOLD: u64 = 64; // This will reseed for every ≤64 session keys
+    /// const RESEED_THRESHOLD: u64 = 4096; // This will reseed for every ≤4096 session keys
     /// let rng = ReseedingRng::<ChaCha12Core, _>::new(RESEED_THRESHOLD * 16, OsRng)?;
     /// let store = RedisStore::<SessionData>::open("redis://127.0.0.1/")
     ///     .await?
