5075f4f1bf2fdb271b5eed73d986b21275fc0369
loqusion <loqusion@gmail.com>
2025-02-23 01:14:31 -0600

    feat(core): `now()` function to return local datetime

--- tower-sesh-core/src/lib.rs
+++ tower-sesh-core/src/lib.rs
@@ -23,3 +23,13 @@ pub mod store;
 const WEEK_IN_SECONDS: u32 = 60 * 60 * 24 * 7;
 /// Default TTL for a session, in seconds.
 pub const DEFAULT_SESSION_EXPIRY_SECONDS: u32 = 2 * WEEK_IN_SECONDS;
+
+/// Returns the current date and time with the local system's UTC offset.
+///
+/// If the system's UTC offset could not be found, then [`now_utc`] is used
+/// instead.
+///
+/// [`now_utc`]: store::Ttl::now_utc
+pub fn now() -> store::Ttl {
+    store::Ttl::now_local().unwrap_or_else(|_| store::Ttl::now_utc())
+}
