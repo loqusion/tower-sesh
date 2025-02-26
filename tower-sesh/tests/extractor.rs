3ad2da7275a817756006d023329665f2c03dedc5
loqusion <loqusion@gmail.com>
2025-02-26 17:52:13 -0600

    test(main): extractor ignores deserialization error

--- tower-sesh/tests/extractor.rs
+++ tower-sesh/tests/extractor.rs
@@ -1,7 +1,21 @@
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+
 use axum::{body::Body, routing, Router};
-use http::Request;
+use http::{header, Request, StatusCode};
 use tower::ServiceExt;
-use tower_sesh::Session;
+use tower_sesh::{Session, SessionLayer};
+use tower_sesh_core::{
+    store::{self},
+    SessionKey,
+};
+
+mod common;
+use common::ErrStore;
+
+const ERROR_MESSAGE: &str = "`ErrStore` always returns an error";
 
 #[tokio::test]
 #[should_panic = "missing request extension"]
@@ -11,4 +25,36 @@ async fn session_extractor_without_layer() {
         .layer(axum::middleware::from_extractor::<Session<()>>());
     let req = Request::builder().uri("/").body(Body::empty()).unwrap();
     let _res = app.oneshot(req).await.unwrap();
+}
+
+#[tokio::test]
+async fn ignores_deserialization_error() {
+    let did_handler = Arc::new(AtomicBool::new(false));
+
+    let did_handler_clone = Arc::clone(&did_handler);
+    let handler = |session: Session<()>| async move {
+        did_handler_clone.store(true, Ordering::SeqCst);
+        assert!(session.get().is_none());
+    };
+
+    let app = Router::new()
+        .route("/", routing::get(handler))
+        .layer(SessionLayer::plain(serde_err_store::<()>()).cookie_name("id"));
+
+    let req = Request::builder()
+        .uri("/")
+        .header(
+            header::COOKIE,
+            format!("id={}", SessionKey::try_from(1).unwrap().encode()),
+        )
+        .body(Body::empty())
+        .unwrap();
+    let res = app.oneshot(req).await.unwrap();
+
+    assert_eq!(res.status(), StatusCode::OK);
+    assert!(did_handler.load(Ordering::SeqCst));
+}
+
+fn serde_err_store<T: Send + Sync + 'static>() -> Arc<ErrStore<T>> {
+    Arc::new(ErrStore::new(|| store::Error::serde(ERROR_MESSAGE)))
 }
