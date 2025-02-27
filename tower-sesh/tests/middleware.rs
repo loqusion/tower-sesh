a1df8ffea9fe2164aaf85f6e1573c6d995bcccb9
loqusion <loqusion@gmail.com>
2025-02-27 06:02:06 -0600

    test(main): `SessionLayer` preserves existing `SetCookie` header

--- tower-sesh/tests/middleware.rs
+++ tower-sesh/tests/middleware.rs
@@ -1,9 +1,10 @@
-use std::sync::Arc;
+use std::{collections::HashSet, sync::Arc};
 
-use axum::{body::Body, routing, Router};
-use http::Request;
+use axum::{body::Body, response::IntoResponse, routing, Router};
+use cookie::Cookie;
+use http::{header, HeaderValue, Request};
 use tower::{ServiceBuilder, ServiceExt};
-use tower_sesh::{store::MemoryStore, SessionLayer};
+use tower_sesh::{store::MemoryStore, Session, SessionLayer};
 
 #[tokio::test]
 #[should_panic = "called more than once!"]
@@ -22,4 +23,38 @@ async fn multiple_session_layers() {
 #[should_panic = "invalid `cookie_name` value"]
 fn invalid_cookie_name() {
     SessionLayer::plain(Arc::new(MemoryStore::<()>::new())).cookie_name("\n");
+}
+
+#[tokio::test]
+async fn preserves_existing_set_cookie() {
+    async fn handler(session: Session<()>) -> impl IntoResponse {
+        session.insert(());
+
+        axum::response::Response::builder()
+            .header(
+                header::SET_COOKIE,
+                Cookie::new("hello", "world")
+                    .encoded()
+                    .to_string()
+                    .parse::<HeaderValue>()
+                    .unwrap(),
+            )
+            .body(Body::empty())
+            .unwrap()
+    }
+
+    let app = Router::new()
+        .route("/", routing::get(handler))
+        .layer(SessionLayer::plain(Arc::new(MemoryStore::<()>::new())).cookie_name("id"));
+
+    let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+    let res = app.oneshot(req).await.unwrap();
+
+    let mut names = HashSet::new();
+    for value in res.headers().get_all(header::SET_COOKIE) {
+        let cookie = Cookie::parse_encoded(value.to_str().unwrap()).unwrap();
+        names.insert(cookie.name().to_owned());
+    }
+
+    assert!(names.contains("hello"));
 }
