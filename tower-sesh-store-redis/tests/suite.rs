5f7c0b254bdbdd451af58339b52215e9d943dba1
loqusion <loqusion@gmail.com>
2025-03-12 00:21:46 -0500

    test(store-redis): fail tests faster on redis connection failure

--- tower-sesh-store-redis/tests/suite.rs
+++ tower-sesh-store-redis/tests/suite.rs
@@ -6,10 +6,12 @@ use std::{
         atomic::{AtomicU64, Ordering::SeqCst},
         LazyLock,
     },
+    time::Duration,
 };
 
 use rand::SeedableRng;
 use rand_chacha::ChaCha20Rng;
+use redis::aio::ConnectionManagerConfig;
 use tower_sesh_store_redis::RedisStore;
 
 static REDIS_URL: LazyLock<&'static str> = LazyLock::new(|| {
@@ -20,7 +22,12 @@ static REDIS_URL: LazyLock<&'static str> = LazyLock::new(|| {
 
 static SEED: AtomicU64 = AtomicU64::new(0);
 
-tower_sesh_test::test_suite!(RedisStore::open(REDIS_URL.clone())
-    .await
-    .unwrap()
-    .rng(ChaCha20Rng::seed_from_u64(SEED.fetch_add(1, SeqCst))));
+tower_sesh_test::test_suite!(RedisStore::with_config(
+    REDIS_URL.clone(),
+    ConnectionManagerConfig::new()
+        .set_connection_timeout(Duration::from_secs(5))
+        .set_number_of_retries(1)
+)
+.await
+.expect("failed to connect to redis")
+.rng(ChaCha20Rng::seed_from_u64(SEED.fetch_add(1, SeqCst))));
