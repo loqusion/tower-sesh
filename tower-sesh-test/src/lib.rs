6b536715bf048796c9160cc99b7a7d3e7fcef22f
loqusion <loqusion@gmail.com>
2025-03-12 00:45:30 -0500

    test(suite): store tests

--- tower-sesh-test/src/lib.rs
+++ tower-sesh-test/src/lib.rs
@@ -1,4 +1,8 @@
-use tower_sesh_core::SessionStore;
+use std::time::Duration;
+
+use rand::{Rng, SeedableRng};
+use rand_chacha::ChaCha20Rng;
+use tower_sesh_core::{time::now, SessionKey, SessionStore};
 
 #[doc(hidden)]
 pub use paste;
@@ -8,7 +12,7 @@ macro_rules! test_suite {
     ($store:expr) => {
         $crate::test_suite! {
             @impl $store =>
-            smoke
+            smoke loading_a_missing_session_returns_none update_creates_missing_entry
         }
     };
 
@@ -25,3 +29,28 @@ macro_rules! test_suite {
 }
 
 pub async fn test_smoke(_store: impl SessionStore<()>) {}
+
+pub async fn test_loading_a_missing_session_returns_none(store: impl SessionStore<()>) {
+    let mut rng = ChaCha20Rng::seed_from_u64(999412874);
+    let session_key = rng.random::<SessionKey>();
+
+    let record = store.load(&session_key).await.unwrap();
+    assert!(record.is_none());
+}
+
+pub async fn test_update_creates_missing_entry(store: impl SessionStore<String>) {
+    let mut rng = ChaCha20Rng::seed_from_u64(56474);
+    let session_key = rng.random::<SessionKey>();
+    let ttl = now() + Duration::from_secs(10);
+
+    store
+        .update(&session_key, &"hello world".to_owned(), ttl)
+        .await
+        .unwrap();
+
+    let record = store.load(&session_key).await.unwrap();
+    assert_eq!(
+        record.as_ref().map(|rec| rec.data.as_str()),
+        Some("hello world")
+    );
+}
