17429bff4416a06ab400511d9165451117fd44c8
loqusion <loqusion@gmail.com>
2025-03-11 23:57:59 -0500

    test: add store to test suite

--- tower-sesh-store-redis/tests/suite.rs
+++ tower-sesh-store-redis/tests/suite.rs
@@ -1,1 +1,11 @@
-tower_sesh_test::test_suite!(());
+use std::{env, sync::LazyLock};
+
+use tower_sesh_store_redis::RedisStore;
+
+static REDIS_URL: LazyLock<&'static str> = LazyLock::new(|| {
+    env::var("REDIS_URL")
+        .expect("`REDIS_URL` environment variable must be set")
+        .leak()
+});
+
+tower_sesh_test::test_suite!(RedisStore::open(REDIS_URL.clone()).await.unwrap());
--- tower-sesh-test/src/lib.rs
+++ tower-sesh-test/src/lib.rs
@@ -1,11 +1,27 @@
+use tower_sesh_core::SessionStore;
+
+#[doc(hidden)]
+pub use paste;
+
 #[macro_export]
 macro_rules! test_suite {
     ($store:expr) => {
-        #[test]
-        fn smoke() {
-            $crate::smoke();
+        $crate::test_suite! {
+            @impl $store =>
+            smoke
         }
+    };
+
+    (@impl $store:expr => $($test:ident)+) => {
+        $(
+            #[tokio::test]
+            async fn $test() {
+                $crate::paste::paste!{
+                    $crate::[<test_ $test>]($store).await;
+                }
+            }
+        )+
     };
 }
 
-pub fn smoke() {}
+pub async fn test_smoke(_store: impl SessionStore<()>) {}
