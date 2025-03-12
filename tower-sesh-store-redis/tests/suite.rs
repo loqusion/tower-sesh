841e38d13e89ca6497d6c8b55b60fc10ea8837af
loqusion <loqusion@gmail.com>
2025-03-12 00:17:37 -0500

    test(store-redis): test with seeded rng

--- tower-sesh-store-redis/tests/suite.rs
+++ tower-sesh-store-redis/tests/suite.rs
@@ -1,5 +1,15 @@
-use std::{env, sync::LazyLock};
+#![cfg(feature = "test-util")]
 
+use std::{
+    env,
+    sync::{
+        atomic::{AtomicU64, Ordering::SeqCst},
+        LazyLock,
+    },
+};
+
+use rand::SeedableRng;
+use rand_chacha::ChaCha20Rng;
 use tower_sesh_store_redis::RedisStore;
 
 static REDIS_URL: LazyLock<&'static str> = LazyLock::new(|| {
@@ -8,4 +18,9 @@ static REDIS_URL: LazyLock<&'static str> = LazyLock::new(|| {
         .leak()
 });
 
-tower_sesh_test::test_suite!(RedisStore::open(REDIS_URL.clone()).await.unwrap());
+static SEED: AtomicU64 = AtomicU64::new(0);
+
+tower_sesh_test::test_suite!(RedisStore::open(REDIS_URL.clone())
+    .await
+    .unwrap()
+    .rng(ChaCha20Rng::seed_from_u64(SEED.fetch_add(1, SeqCst))));
