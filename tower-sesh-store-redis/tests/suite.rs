93e60534a9fbc326a1512b82f27e1c0045dde9f9
loqusion <loqusion@gmail.com>
2025-03-12 00:26:30 -0500

    test(store-redis): move store initialization to `store` function

--- tower-sesh-store-redis/tests/suite.rs
+++ tower-sesh-store-redis/tests/suite.rs
@@ -12,6 +12,8 @@ use std::{
 use rand::SeedableRng;
 use rand_chacha::ChaCha20Rng;
 use redis::aio::ConnectionManagerConfig;
+use serde::{de::DeserializeOwned, Serialize};
+use tower_sesh_core::SessionStore;
 use tower_sesh_store_redis::RedisStore;
 
 static REDIS_URL: LazyLock<&'static str> = LazyLock::new(|| {
@@ -22,12 +24,19 @@ static REDIS_URL: LazyLock<&'static str> = LazyLock::new(|| {
 
 static SEED: AtomicU64 = AtomicU64::new(0);
 
-tower_sesh_test::test_suite!(RedisStore::with_config(
-    REDIS_URL.clone(),
-    ConnectionManagerConfig::new()
-        .set_connection_timeout(Duration::from_secs(5))
-        .set_number_of_retries(1)
-)
-.await
-.expect("failed to connect to redis")
-.rng(ChaCha20Rng::seed_from_u64(SEED.fetch_add(1, SeqCst))));
+async fn store<T>() -> impl SessionStore<T>
+where
+    T: Serialize + DeserializeOwned + Send + Sync + 'static,
+{
+    RedisStore::with_config(
+        REDIS_URL.clone(),
+        ConnectionManagerConfig::new()
+            .set_connection_timeout(Duration::from_secs(5))
+            .set_number_of_retries(1),
+    )
+    .await
+    .expect("failed to connect to redis")
+    .rng(ChaCha20Rng::seed_from_u64(SEED.fetch_add(1, SeqCst)))
+}
+
+tower_sesh_test::test_suite!(store().await);
