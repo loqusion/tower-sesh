66d9e48234ce52c407afb7762e0e1ff452010d77
loqusion <loqusion@gmail.com>
2025-03-19 03:53:56 -0500

    docs(core): tracing macros are a no-op without the `tracing` feature

--- tower-sesh-core/src/macros.rs
+++ tower-sesh-core/src/macros.rs
@@ -1,6 +1,8 @@
 /// Logs a message at the error level.
 ///
 /// This macro delegates to [`tracing::error`].
+///
+/// If the `tracing` feature is not enabled, this is a no-op.
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
@@ -12,6 +14,8 @@ macro_rules! error {
 /// Logs a message at the warn level.
 ///
 /// This macro delegates to [`tracing::warn`].
+///
+/// If the `tracing` feature is not enabled, this is a no-op.
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
@@ -23,6 +27,8 @@ macro_rules! warn {
 /// Logs a message at the info level.
 ///
 /// This macro delegates to [`tracing::info`].
+///
+/// If the `tracing` feature is not enabled, this is a no-op.
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
@@ -34,6 +40,8 @@ macro_rules! info {
 /// Logs a message at the debug level.
 ///
 /// This macro delegates to [`tracing::debug`].
+///
+/// If the `tracing` feature is not enabled, this is a no-op.
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
@@ -45,6 +53,8 @@ macro_rules! debug {
 /// Logs a message at the trace level.
 ///
 /// This macro delegates to [`tracing::trace`].
+///
+/// If the `tracing` feature is not enabled, this is a no-op.
 #[macro_export]
 macro_rules! trace {
     ($($arg:tt)*) => {
